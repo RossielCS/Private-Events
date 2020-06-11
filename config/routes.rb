d97112524b7877d89c1798efe6fbb910169f20d5
Rossiel Carranza <rossielcarranza@gmail.com>
2020-06-11 18:19:05 -0300

    Add sessions controller
    - New, create, destroy sessions methods
    - log_in, log_out, current_user methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
   # get 'users/new'
   # get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup'  => 'users#new'  
   resources :users, only: [:new, :create, :show]
 
-  root 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  root 'sessions#new'
 end

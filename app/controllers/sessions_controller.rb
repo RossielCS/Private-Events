27bd6e7c9e4d728bf1bd7029908744aee59e1e83
Rossiel Carranza <rossielcarranza@gmail.com>
2020-06-13 00:18:58 -0300

    Add links in layouts app
    - Links to jump between views
    - Fix rubocop offenses

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in(user)
       redirect_to user
     else
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,19 +1,18 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get events_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get events_show_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get events_new_url
     assert_response :success
   end
-
 end
